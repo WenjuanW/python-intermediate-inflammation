049f21c5a70b14c4e04650cc4bcafd701e74f2aa
WenjuanWang <wwang.elsie@gmail.com>
2023-05-26 18:57:51 +0100

    Correct style errors

--- inflammation/models.py
+++ inflammation/models.py
@@ -21,7 +21,8 @@ def load_csv(filename):
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
 
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data
+    (each row contains measurements for a single patient across all days).
     :returns: An array of mean values of measurements for each day.
     """
     return np.mean(data, axis=0)
@@ -29,7 +30,8 @@ def daily_mean(data):
 
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data
+    (each row contains measurements for a single patient across all days).
     :returns: An array of max values of measurements for each day.
     """
     return np.max(data, axis=0)
@@ -38,7 +40,8 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
 
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data
+    (each row contains measurements for a single patient across all days).
     :returns: An array of min values of measurements for each day.
     """
     return np.min(data, axis=0)
@@ -53,16 +56,16 @@ def patient_normalise(data):
     :param data: 2D array of inflammation data
     :type data: ndarray
     """
-    if not ininstance(data, np.ndarray):
+    if not isinstance(data, np.ndarray):
         raise TypeError('data input should be ndarray')
     if len(data.shape) != 2:
         raise ValueError('inflammation array should be 2-dimensional')
     if np.array(data < 0):
         raise ValueError('Inflammation values should not be negative')
 
-    max = np.nanmax(data, axis=1)
+    max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     return normalised
 
